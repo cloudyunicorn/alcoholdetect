b6c8a6262e5022a13103ed60f106f6aa2743bd7a
Rajat Singh <s.rajat55@gmail.com>
2026-06-17 13:09:43 +0530

    check indexnow

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import indexnow from 'astro-indexnow';
 
 // Load environment variables (reads from .env files)
 const env = loadEnv(process.env.NODE_ENV || 'production', process.cwd(), '');
-const INDEXNOW_KEY = env.INDEXNOW_KEY || process.env.INDEXNOW_KEY || '8521d9be856644f192b0c5cb2cb7ffc7';
+const INDEXNOW_KEY = env.INDEXNOW_KEY || process.env.INDEXNOW_KEY || '9867a9975dcf4e368ebd56f3941b9238';
 
 // https://astro.build/config
 export default defineConfig({
