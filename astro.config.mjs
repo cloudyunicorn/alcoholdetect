aee8c475385afbdeaf5d504ea64ef0c90a00c0e3
Rajat Singh <s.rajat55@gmail.com>
2026-06-17 12:27:19 +0530

    added astro-indexnow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import indexnow from 'astro-indexnow';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.alcoholdetect.com',
+
   server: {
     host: true
   },
@@ -12,6 +15,14 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'always'
   },
+
+  integrations: [
+    indexnow({
+      // This automatically updates Bing every single time your build pipeline runs!
+      hostname: 'www.alcoholdetect.com',
+      enabled: true,
+    })
+  ],
 
   vite: {
     plugins: [tailwindcss()]
